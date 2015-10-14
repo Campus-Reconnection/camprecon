a5a2137cbc169d3ff6b087d586e544be092018eb
Buster Schrader <bstrdash@gmail.com>
2015-10-13 20:49:36 -0500

    new file:   images/rndblu/border_e.png 	new file:   images/rndblu/border_n.png 	new file:   images/rndblu/border_ne.png 	new file:   images/rndblu/border_nw.png 	new file:   images/rndblu/border_s.png 	new file:   images/rndblu/border_se.png 	new file:   images/rndblu/border_sw.png 	new file:   images/rndblu/border_w.png 	modified:   includes/quicklist.php 	modified:   includes/weeklyschedule.php 	modified:   map.php 	modified:   style.css

--- includes/quicklist.php
+++ includes/quicklist.php
@@ -1,8 +1,9 @@
-<div id="quicklist">
 Quick List:
+<div id="quicklist"><table class="win_table"><tr><td class="win_nwcorner"></td><td class="win_nborder"></td><td class="win_necorner"></td></tr><tr><td class="win_wborder"></td><td class="win_center">
 <table>
 <tr><td>&nbsp;Future Due:</td><td>$4,230.54</td></tr>
 <tr><td>&nbsp;Advisor:</td><td>Lancelot Supersad Ph. D -- (701) 867-5309</td></tr>
+<tr><td>&nbsp;</td></tr>
 <tr><td>&nbsp;ET CETERA...</td></tr>
 </table>
-</div>
+</td><td class="win_eborder"></td></tr><tr><td class="win_swcorner"></td><td class="win_sborder"></td><td class="win_secorner"></td></tr></table></div>
--- map.php
+++ map.php
@@ -1,3 +1,20 @@
+<?php
+function addmarker($Name,$Var,$Latitude,$Longitude)
+{
+	//-----------------------------------------------------------------//
+	// Use me to place markers on the map.                             //
+	//-----------------------------------------------------------------//
+	// $Name is title of the building.                                 //
+	// $Var is just basically a call sign, its used in the javascript. //
+	// $Latitude and $Longitude are x and y coordinates respectively.  //
+	//-----------------------------------------------------------------//
+	
+	echo "var " . $Var . "marker = new google.maps.Marker({position:{lat:" . $Latitude . ", lng:" . $Longitude . "}, map:map, title:\"" . $Name . "\"});\r\n";
+	echo "var " . $Var . "info = new google.maps.InfoWindow({content:\"" . $Name . "\"}); " . $Var . "info.open(map," . $Var . "marker);\r\n";
+	echo "google.maps.event.addListener(" . $Var . "marker,\"click\",function(){" . $Var . "info.open(map," . $Var . "marker);});\r\n";
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,18 +22,25 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 <script src="http://maps.googleapis.com/maps/api/js"></script>
 <script>
-function initialize()
+function initMap()
 {
-  var mapProp = {
-    center:new google.maps.LatLng(46.896824, -96.8033365),
-    zoom:15,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
+  var mapProp =
+  {
+    center: new google.maps.LatLng(46.894080,-96.802780),
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   };
- 
+  
   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  
+  //Markers go here:
+  <?php 
+    addmarker("Quentin Burdick Building","qbb",46.893617,-96.803128);
+	addmarker("Minard Hall","minard",46.891419,-96.802421);
+  ?> 
 }
 
-google.maps.event.addDomListener(window,"load",initialize);
+google.maps.event.addDomListener(window,"load",initMap);
 </script>
 </head>
 <body>
