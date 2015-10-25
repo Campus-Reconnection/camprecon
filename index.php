2979e5812d48170d4d838596f6c3846c09b5cc87
nbirk <nicole.c.birkholz@ndsu.edu>
2015-10-25 15:24:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <img src="images/campusreconnectionlogo.png" />
 <div id="pagediv">
 <?php include("includes/menustrip.php"); ?>
+<header>
 <ul class="topnav">
   <li><a href="#" style="color: white;">Home</a></li>
   <li><a href="#">Personal</a></li>
@@ -15,6 +16,7 @@
   <li><a href="#">Expenses</a></li>
   <li><a href="#">Other</a></li>
   </ul>
+</header>
 <span class="title">Weekly Schedule:</span>
 <br /><br />
 <?php include("includes/weeklyschedule.php"); ?>
