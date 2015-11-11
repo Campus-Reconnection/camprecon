7d9c34e351889e5e9edf0c1126b747b49b53896c
Erik Canton <erik.canton@ndsu.edu>
2015-11-11 14:47:49 -0600

    more queries

---
+++ library/classqueries.php
@@ -0,0 +1,135 @@
+<?php
+
+/*
+This Class contains query methods for the 
+'Class Management' section of the website
+*/
+
+// Server and DB Information
+$servername = "134.129.125.206";
+$username = "ecanton";
+$password = "FishPants2015";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+/*
+// Search for class by class ID 
+// param: class ID ( CSCI222, STAT360, ...)
+// return: ID, Name, and Section
+*/
+function classSearchID($id) {
+	$sql = "SELECT strCourseID, strCourseName, intSectionNumber
+		FROM tblCourse
+		WHERE tblCourse.strCourseID = $id
+		INNER JOIN tblSection
+		ON tblSection.strCourseID = tblCourse.strCourseID";
+	$result = $conn->query($sql);
+	return $result;
+
+}
+
+/*
+// Get class sections from department name
+// param: department name (Computer Science, Psychology, ...)
+// return: ID, Name, and Section
+*/
+function classSearchDep($dep) {
+	$sql = "SELECT strCourseID, strCourseName, intSectionNumber
+		FROM tblCourse
+		WHERE tblCourse.strDepartment = $dep
+		INNER JOIN tblSection
+		ON tblSection.strCourseID = tblCourse.strCourseID";
+	$result = $conn->query($sql);
+	return $result;
+}
+
+/*
+// Get the scheduling information about a certain section
+// param: sectionid (123456789098, ...)
+// return: start time, end time, days
+*/
+function getSectionTime($sectionid) {
+	$sql = "SELECT intSectionTimeStart, intSectionTimeEnd, strDayPattern
+		FROM tblSectionTime, tblSectionDayPattern
+		WHERE tblSection.sectionID = $sectionid";
+	$result = $conn->query($sql);
+	return $result;
+}
+
+/*
+// Get the date the section is scheduled for
+// param: sectionid (123456789098, ...)
+// return: start date, end date
+*/
+function getSectionDate($sectionid) {
+	$sql = "SELECT dtmStart, dtmEnd
+		FROM tblSection
+		WHERE sectionID = $sectionid";
+	$result = $conn->query($sql);
+	return $result;
+}
+
+/*
+// Get the professors name for the section
+// param: sectionid (123456789098, ...)
+// return: first name, last name
+*/
+function getSectionProf($sectionid) {
+	$sql = "SELECT strFirstName, strLastName
+		FROM tblFaculty
+		WHERE tblSection.intSectionID = $sectionid 
+		AND tblSection.intFacultyID = tblFaculty.intFacultyID";
+	$result = $conn->query($sql);
+	return $result;
+}
+
+/*
+// Get the number of students allowed in a section
+// param: sectionid (123456789098, ...)
+// return: capacity
+*/
+function getSectionCapacity($sectionid) {
+	$sql = "SELECT intCapacity
+		FROM tblSection
+		WHERE intSectionID = $sectionid";
+	$result = $conn->query($sql);
+	return $result;
+
+}
+
+/*
+// Get a description of the course
+// param: course id (CSCI222, STAT360, ...)
+// return: course description
+*/
+function getCourseDescription($courseID) {
+	$sql = "SELECT strCourseDescription
+		FROM tblCourse
+		WHERE strCourseID = $courseID";
+	$result = $conn->query($sql);
+	return $result;
+}
+
+/*
+// Get the location of the section
+// param: sectionid (123456789098, ...)
+// return: building and room number
+*/
+function getSectionRoom($sectionid) {
+	$sql = "SELECT strFacilityName, intRoomNumber
+		FROM tblFacility, tblRoom
+		WHERE tblSection.intSectionID = $sectionID
+		AND tblSection.intRoomID = tblRoom.intRoomID
+		AND tblRoom.intFacilityID = tblFacility.intFacilityID";
+	$result = $conn->query($sql);
+	return $result;
+}
+
+?>
