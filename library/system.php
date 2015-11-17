66f321a1283edbbf727956d0132109ac5c0f51b5
Buster Schrader <bstrdash@gmail.com>
2015-11-17 16:31:58 -0600

    modified:   library/system.php

--- library/system.php
+++ library/system.php
@@ -29,30 +29,5 @@
 		closeDB($conn);
 		return $result;
 	}
-	
-	function array_to_xml($object_info, $xml_object_info)
-	{
-		foreach ($object_info as $key => $value)
-		{
-			$lcKey = strtolower($key);
-			if (is_array($value))
-			{
-				if (!is_numeric($lcKey))
-				{
-					$subnode = $xml_object_info->addChild("$lcKey");
-					array_to_xml($value, $subnode);
-				}
-				else
-				{
-					$subnode = $xml_object_info->addChild("item");
-					array_to_xml($value, $subnode);
-				}
-			}
-			else
-			{
-				$xml_object_info->addChild("$lcKey",htmlspecialchars("$value"));
-			}
-		}
-	}
 
 ?>
