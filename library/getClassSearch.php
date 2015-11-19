a35ae19bca768f3a2855b53ad136658391442370
Buster Schrader <bstrdash@gmail.com>
2015-11-19 15:47:58 -0600

    Class search now works.

--- library/getClassSearch.php
+++ library/getClassSearch.php
@@ -1,53 +1,69 @@
 <?php
-//require("system.php");
 
-function searchCourses($course) {
-
-$sql = "SELECT strCourseName as courseName,
-			intSectionID as secID,
-			intSectionNumber as secNumber,
-			dtmStart, dtmEnd,
-			strFirstName, strLastName,
-			strFacilityName, strRoomNumber
-			FROM tblCourse
-			INNER JOIN tblsection ON tblcourse.strCourseID = tblsection.strCourseID
-			INNER JOIN tblfaculty ON tblsection.intFacultyID = tblfaculty.intFacultyID
-			INNER JOIN tblroom ON tblsection.strRoomID = tblRoom.intRoomID
-			INNER JOIN tblfacility ON tblroom.intFacilityID = tblFacility.intFacilityID
-			WHERE tblcourse.strCourseID like '$course'";
-
-$result = queryDB($sql);
-
-if (mysqli_num_rows($result) > 0)
+function searchCourses($search)
 {
-	foreach($rows as $row) {
-		echo "<tr>";
-			echo "<td>".$row['courseName']."</td>";
-			echo "<td>".$row['secID']."</td>";
-			echo "<td>".$row['secNumber']."</td>";
-			echo "<td>".$row['dtmStart']."</td>";
-			echo "<td>".$row['dtmEnd']."</td>";
-			echo "<td>".$row['strFirstName']."</td>";
-			echo "<td>".$row['strLastName']."</td>";
-			echo "<td>".$row['strFacilityName']."</td>";
-			echo "<td>".$row['intRoomNumber']."</td>";
-			echo "<td> <input type=\"checkbox\" name=\"check[".$row['secID']."]\" value=\"\" id=\"checkbox\" /> </td>";
-		echo "</tr>";	
+	$sql = "SELECT DISTINCT strCourseName AS courseName,
+		intSectionID AS secID,
+		intSectionNumber AS secNumber,
+		dtmStartDate,
+		dtmEndDate,
+		strFirstName,
+		strLastName,
+		strFacilityName,
+		strRoomNumber
+		FROM tblCourse
+		INNER JOIN tblsection ON tblcourse.strCourseID = tblsection.strCourseID
+		INNER JOIN tblfaculty ON tblsection.intFacultyID = tblfaculty.intFacultyID
+		INNER JOIN tblroom ON tblsection.strRoomID = tblRoom.intRoomID
+		INNER JOIN tblfacility ON tblroom.intFacilityID = tblFacility.intFacilityID
+		WHERE tblcourse.strCourseID LIKE '$search' OR
+			tblcourse.strDeptCode LIKE '$search' OR
+			tblfaculty.strLastName LIKE '$search' OR
+			tblfaculty.strFirstName LIKE '$search'";
+
+	$result = queryDB($sql);
+
+	if (mysqli_num_rows($result) > 0)
+	{
+		echo "<thead>";
+		echo "<tr><td class=\"thr\">Select</td>";
+		echo "<td class=\"thr\">Course Name</td>";
+		echo "<td class=\"thr\">Section</td>";
+		echo "<td class=\"thr\">Start Time</td>";
+		echo "<td class=\"thr\">End Time</td>";
+		echo "<td class=\"thr\">Instructor</td>";
+		echo "<td class=\"thr\">Facility</td>";
+		echo "<td class=\"thr\">Room</td></tr>";
+		echo "</thead><tbody><tr>";
+		
+		while($row = mysqli_fetch_assoc($result))
+		{
+			echo "<td class=\"advcell\"><input type=\"checkbox\" name=\"check[".$row['secID']."]\" value=\"\" /></td>";
+			echo "<td class=\"advcell\">".$row['courseName']."</td>";
+			echo "<td class=\"advcell\">".$row['secNumber']."</td>";
+			echo "<td class=\"advcell\">".$row['dtmStartDate']."</td>";
+			echo "<td class=\"advcell\">".$row['dtmEndDate']."</td>";
+			echo "<td class=\"advcell\">".$row['strFirstName'].' '.$row['strLastName']."</td>";
+			echo "<td class=\"advcell\">".$row['strFacilityName']."</td>";
+			echo "<td class=\"advcell\">".$row['strRoomNumber']."</td>";
+			echo "</tr></tbody>";	
+		}
+	}
+	else
+	{
+		echo "<tr><td class=\"advcell\">Your search returned no results.</td></tr>";
 	}
 }
 
-}
-
-function addCourse($secID) {
-
-	$sql = "INSERT INTO tblstudentenrollment VALUES(
-		$secID)";
-
+function addCourse($secID)
+{
+	$sql = "INSERT INTO tblstudentenrollment VALUES($secID)";
 	$result = queryDB($sql);
 }
 
-function getAllDepartments() {
-	$sql = "SELECT * FROM `tbldepartment` ORDER BY `tbldepartment`.`strDeptCode` ASC";
+function getAllDepartments()
+{
+	$sql = "SELECT * FROM tbldepartment ORDER BY strDeptCode ASC";
 	return $sql;
 }
 
