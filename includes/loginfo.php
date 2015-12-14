cbae73cb8b42344b6d8efa321d38123ad4979eb1
Buster Schrader <bstrdash@gmail.com>
2015-12-14 17:57:52 -0600

    modified:   includes/loginfo.php 	modified:   includes/quicklist.php

--- includes/loginfo.php
+++ includes/loginfo.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['crlogin']) && $_SESSION['crlogin'] != '')
 {
 	echo '<table id="loginfoTable"><tr>';
 	echo '<td class="loginfo">Welcome,<br />'.$_SESSION['crname'].'!</td>';
-	echo '<td class="loginfo"><image src="images/'.$_SESSION['crphototb'].'" id="loginfoPhoto"></image></td>';
+	echo '<td class="loginfo"><img src="images/'.$_SESSION['crphototb'].'" id="loginfoPhoto" /></td>';
 	echo '<td class="loginfo"><form method="POST" action="includes/logout.php">';
 	echo '<input type="image" src="images/powerbutton.png" alt="Logout"; />';
 	echo '</form></td></tr></table>';
--- includes/quicklist.php
+++ includes/quicklist.php
@@ -1,14 +1,41 @@
-<table id="quicklist">
-<tr><td>&nbsp;ACCOUNT HOLD:</td><td>Financial Obligation Agreement</td></tr>
-<tr><td>&nbsp;</td><td>&nbsp;</td></tr>
-<tr><td>&nbsp;Future Due:</td><td>$4,230.54 <span class="text-attn">(Pay your bills deadbeat!)</span></td></tr>
-<tr><td>&nbsp;Advisor:</td><td>Lancelot Supersad Ph. D -- (701) 867-5309</td></tr>
-<tr><td>&nbsp;</td><td>&nbsp;</td></tr>
-<tr><td>&nbsp;Contact Info:</td><td>&nbsp;</td></tr>
-<tr><td>&nbsp;&nbsp;-Address:</td><td>2**4 E. L******e R**d</td></tr>
-<tr><td>&nbsp;&nbsp;-City:</td><td>Jamestown, ND 58401-9603</td></tr>
-<tr><td>&nbsp;&nbsp;-Phone:</td><td>(701)-2*9-2**5</td></tr>
-<tr><td>&nbsp;&nbsp;-Email:</td><td>b******h@gmail.com</td></tr>
-<tr><td>&nbsp;</td><td>&nbsp;</td></tr>
-<tr><td>&nbsp;Outages:</td><td>None planned at the moment.</td></tr>
-</table>
+<?php
+
+$conn = openDB();
+
+echo "<table id=\"quicklist\">\r\n";
+echo "<tr><td>&nbsp;ACCOUNT HOLD:</td><td>Financial Obligation Agreement</td></tr>\r\n";
+echo "<tr><td>&nbsp;</td><td>&nbsp;</td></tr>\r\n";
+
+$sql = "SELECT dblDues FROM tblstudent WHERE strStudentEID = '" . $_SESSION["cruser"] . "';";
+$result = queryDB($sql);
+$row = mysqli_fetch_assoc($result);
+
+echo "<tr><td>&nbsp;Future Due:</td><td>$" . $row["dblDues"] . " <span class=\"text-attn\">(Pay your bills deadbeat!)</span></td></tr>\r\n";
+
+$sql = "SELECT CONCAT(fac.strFirstName,' ',fac.strLastName) AS \"strAdvisor\", fac.strPhone AS \"strAdvPhone\"
+		FROM tblstudent stu
+		JOIN tblfaculty fac ON stu.intFacultyID = fac.intFacultyID
+		WHERE strStudentEID = '" . $_SESSION["cruser"] . "';";
+$result = queryDB($sql);
+$row = mysqli_fetch_assoc($result);
+
+echo "<tr><td>&nbsp;Advisor:</td><td>" . $row["strAdvisor"] . " -- " . preg_replace("/(\\d{3})(\\d{3})(\\d{4})/","(\\1) \\2-\\3",$row["strAdvPhone"]) . "</td></tr>\r\n";
+echo "<tr><td>&nbsp;</td><td>&nbsp;</td></tr>\r\n";
+
+$sql = "SELECT strStreet, CONCAT(strCity,', ',strState,' ',strPostCode) AS \"strUserAddress\", strMobileNumber, strEmail
+		FROM tblusercontact WHERE blnPermanent = true AND strExternalEID = '" . $_SESSION["cruser"] . "';";
+$result = queryDB($sql);
+$row = mysqli_fetch_assoc($result);
+
+echo "<tr><td>&nbsp;Contact Info:</td><td>&nbsp;</td></tr>\r\n";
+echo "<tr><td>&nbsp;&nbsp;-Address:</td><td>" . preg_replace("/\\d{3,4}\\s/", "*** ",$row["strStreet"]) . "</td></tr>\r\n";
+echo "<tr><td>&nbsp;&nbsp;-City:</td><td>" . $row["strUserAddress"] . "</td></tr>\r\n";
+echo "<tr><td>&nbsp;&nbsp;-Phone:</td><td>" . preg_replace("/(\\d).(\\d)(\\d).(\\d)(\\d)..(\\d)/","(\\1*\\2)\\3*\\4-\\5**\\6",$row["strMobileNumber"]) . "</td></tr>\r\n";
+echo "<tr><td>&nbsp;&nbsp;-Email:</td><td>" . preg_replace("/([A-Za-z])\\w+(.)@/", "\\1*\\2@", $row["strEmail"]) . "</td></tr>\r\n";
+echo "<tr><td>&nbsp;</td><td>&nbsp;</td></tr>";
+echo "<tr><td>&nbsp;Outages:</td><td>None planned at the moment.</td></tr>";
+echo "</table>";
+
+closeDB($conn);
+
+?>
