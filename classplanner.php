69a458df80cb50a0823fa452f557b610734cd799
Alex <alexjob222@gmail.com>
2015-12-15 00:38:17 -0600

    Updated Planner Page
    Planner page has basic table giving classes that need to be taken for
    Computer Science major

--- classplanner.php
+++ classplanner.php
@@ -19,8 +19,66 @@ function openMap()
 <?php include("includes/menustrip.php"); ?>
 <span class="title">Class Planner:</span>
 <br /><br />
-<img src="images/menatwork.png" width="128px;" />
-<br /><br />
+<table class="plantable">
+<tr>
+<td class="planblack" colspan="5">GENERAL EDUCATION REQUIREMENTS - 40 Credits Required</td>
+</tr><tr>
+<td class="planhead">Course</td><td class="planhead">Number</td><td class="planhead">Course Title</td><td class="planhead">Credits</td><td class="planhead">Grade</td>
+</tr><tr>
+<td colspan="5" class="plangrey">First Year Experience(F) - 1 credit</td>
+</tr><tr>
+<td class="planreg">UNIV</td><td class="planreg">189</td><td class="planreg">Skills for Academic Success</td><td class="planregcenter">1</td><td class="planregcenter">A</td>
+</tr><tr>
+<td colspan="5" class="plangrey">Communication (C) - 12 credits</td>
+</tr><tr>
+<td class="planreg">ENGL</td><td class="planreg">110</td><td class="planreg">College Composition I</td><td class="planregcenter">3</td><td class="planregcenter">B</td>
+</tr><tr>
+<td class="planreg">ENGL</td><td class="planreg">120</td><td class="planreg">College Composition II</td><td class="planregcenter">3</td><td class="planregcenter"></td>
+</tr><tr>
+<td class="planreg">COMM</td><td class="planreg">110</td><td class="planreg">Fundamentals of Public Speaking</td><td class="planregcenter">3</td><td class="planregcenter">B</td>
+</tr><tr>
+<td class="planreg">ENGL</td><td class="planreg">321 or 324</td><td class="planreg">(Upper-Division Writing)</td><td class="planregcenter">3</td><td class="planregcenter"></td>
+</tr><tr>
+<td colspan="5" class="plangrey">Quantitative Reasoning (R) - 3 credits</td>
+</tr><tr>
+<td class="planreg">Math</td><td class="planreg">165</td><td class="planreg">Calculus I</td><td class="planregcenter">4</td><td class="planregcenter">C</td>
+</tr><tr>
+<td colspan="5" class="plangrey">Science & Technology (S) - 10 credits</td>
+</tr><tr>
+<td class="planreg"></td><td class="planreg"></td><td class="planreg"></td><td class="planregcenter">4</td><td class="planregcenter"></td>
+</tr><tr>
+<td class="planreg"></td><td class="planreg"></td><td class="planreg"></td><td class="planregcenter">3</td><td class="planregcenter"></td>
+</tr><tr>
+<td class="planreg"></td><td class="planreg"></td><td class="planreg"></td><td class="planregcenter">3</td><td class="planregcenter"></td>
+</tr><tr>
+<td colspan="5" class="plangrey">Humanities & Fine Arts (A) - 6 credits</td>
+</tr><tr>
+<td class="planreg"></td><td class="planreg"></td><td class="planreg"></td><td class="planregcenter">3</td><td class="planregcenter"></td>
+</tr><tr>
+<td class="planreg"></td><td class="planreg"></td><td class="planreg"></td><td class="planregcenter">3</td><td class="planregcenter"></td>
+</tr><tr>
+<td colspan="5" class="plangrey">Social & Behavioral Sciences (B) - 6 credits</td>
+</tr><tr>
+<td class="planreg"></td><td class="planreg"></td><td class="planreg"></td><td class="planregcenter">3</td><td class="planregcenter"></td>
+</tr><tr>
+<td class="planreg"></td><td class="planreg"></td><td class="planreg"></td><td class="planregcenter">3</td><td class="planregcenter"></td>
+</tr><tr>
+<td colspan="5" class="plangrey">Wellness (W) - 2 credits</td>
+</tr><tr>
+<td class="planreg"></td><td class="planreg"></td><td class="planreg"></td><td class="planregcenter">2</td><td class="planregcenter"></td>
+</tr><tr>
+<td colspan="5" class="plangrey">Cultural Diversity (D)</td>
+</tr><tr>
+<td class="planreg"></td><td class="planreg"></td><td class="planreg"></td><td class="planregcenter">3</td><td class="planregcenter"></td>
+</tr><tr>
+<td colspan="5" class="plangrey">Global Perspectives (G)</td>
+</tr><tr>
+<td class="planreg"></td><td class="planreg"></td><td class="planreg"></td><td class="planregcenter">3</td><td class="planregcenter"></td>
+</tr>
+</table>
+<!--<img src="images/menatwork.png" width="128px;" />
+<br />-->
+<br />
 <input type="button" value="Open Map" onclick="openMap()" />
 </div>
 </div>
